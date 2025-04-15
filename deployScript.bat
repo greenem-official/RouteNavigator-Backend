ca26444ec08900d7ce79e470ad139ab92d04ef8c
Ivan <vanyapopov2004@gmail.com>
2025-04-15 22:44:15 +0300

    Deploying 1.1.0

--- deployScript.bat
+++ deployScript.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set VERSION=1.0.0
+set VERSION=1.1.0
 
 echo Building the project...
 call ./gradlew build -x test --stacktrace
--- deployScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION="1.0.0"
-
-echo "Building the project..."
-./gradlew build -x test --stacktrace
-
-echo "Deploying the project"
-docker build -t d4ylight/route-navigation-backend:$VERSION -t d4ylight/route-navigation-backend:latest . --target makeImage --no-cache
